b6d06a7e6c89088673135c5e1bc2dd0e1441e229
qqnoodle <foongqiming@gmail.com>
2026-05-01 22:22:48 +0800

    modified the options of lspconfig to allow auto complete by keypress

--- config/packageManager.lua
+++
@@ -1,4 +0,0 @@
-vim.pack.add({"https://github.com/neovim/nvim-lspconfig"})
-vim.pack.add({"https://github.com/mason-org/mason.nvim"})
-vim.pack.add({"https://github.com/mason-org/mason-lspconfig.nvim"})
-vim.pack.add({"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim"})
